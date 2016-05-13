2063478aac9a23ac8206b6a96b20b2593fcd2efc
tntvu <tntvu@tma.com.vn>
2016-05-13 17:19:14 +0700

    trying pixel to hex

--- scenes/map.gd
+++ scenes/map.gd
@@ -18,8 +18,36 @@ func _input(event):
 		print("clicked on: ", get_map_pos(get_global_mouse_pos() - map.get_global_pos()))
 		
 func get_map_pos(mouse_pos):
-	var col = int(mouse_pos.x/map.tile_width)
-	var row = int(mouse_pos.y/(map.tile_height-map.tile_height_offset))
+	var row = int(mouse_pos.y/map.tile_height)
+	var col
+	
+	var row_is_odd = false
+	if row % 2 == 1:
+		row_is_odd = true
+	
+	if row_is_odd:
+		col = int((mouse_pos.x-map.tile_width_offset)/map.tile_width)
+	else:
+		col = int(mouse_pos.x/map.tile_width)
+	
+	var rel_y = mouse_pos.y - (row * map.tile_height)
+	var rel_x
+	
+	if row_is_odd:
+		rel_x = (mouse_pos.x - (col * map.tile_width)) - map.tile_width_offset
+	else:
+		rel_x = mouse_pos.x - (col * map.tile_width)
+		
+	var m = map.tile_height_offset/map.tile_width_offset
+	if (rel_y < (-m * rel_x) + map.tile_height_offset):
+		row -= 1
+		if row_is_odd:
+			col -= 1
+	else:
+		if (rel_y < (m * rel_x) - map.tile_height_offset):
+			row -= 1
+			if row_is_odd:
+				col += 1
 	
 	var node = map.get_node(str("tile_",col,"_",row))
 	if node != selected_node:
