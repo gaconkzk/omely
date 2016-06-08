bfe9a3e7393c4ea696e6db74eb86d7c6cea180ef
tntvu <tntvu@tma.com.vn>
2016-06-08 10:50:53 +0700

    top menu buttons and click on quit worked

--- scripts/menu_controller.gd
+++ scripts/menu_controller.gd
@@ -1,5 +1,7 @@
 var root
 
+var quit_button
+
 var root_tree
 
 var background_gradient
@@ -10,3 +12,14 @@ func init_root(root_node):
 	
 func _ready():
 	self.background_gradient = self.get_node('vigette/center/bg_mask')
+	
+	quit_button = get_node("top/center/quit")
+	
+	quit_button.connect("pressed", self, "_quit_button_pressed")
+	
+func _quit_button_pressed():
+    # self.root.sound_controller.play('menu')
+    self.quit_game()
+
+func quit_game():
+    OS.get_main_loop().quit()
